0179f96c339f03eadda94b1bba9840a0c65bce55
Ossyence <110287364+Ossyence@users.noreply.github.com>
2022-09-06 19:43:28 +0200

    Email complete
    Finish!!!!

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ function sendEmail() {
     const name = document.getElementById("name").value
     const subject = document.getElementById("subject").value
     const body  = document.getElementById("body").value
+    const contactEmail  = document.getElementById("contactEmail").value
 
     Email.send({
         Host: "smtp.elasticemail.com",
         Username : "ossyence@gmail.com",
         Password : "59B00F3B623A4258354302AF327F4B7890E2",
-        To : 'ossyence@gmail.com',
+        To : 'ossyence.emails.recipient@gmail.com',
         From : 'ossyence.emails@gmail.com',
         Subject : subject,
-        Body : body + "\nBy: " + name 
+        Body : "<h3>" + body + "</h3><h2><br><br>By: " + name + "<br>Email: " + contactEmail + "<h2>"
     }).then(
         message => alert(message)
     );
-}
+}s
