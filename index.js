78474a565867354fc80d4087c75d1a7e62cd778b
Ossyence <110287364+Ossyence@users.noreply.github.com>
2022-09-07 07:03:47 +0200

    timer

--- index.js
+++ index.js
@@ -1,37 +1,63 @@
+function convMinToMilli(minutes) {
+    return (1000 * 60 * minutes)
+}
+
+function convSecondsToMilli(seconds) {
+    return (1000 * seconds)
+}
+
+function convHoursToMilli(hours) {
+    return (1000 * 60 * 60 * hours)
+}
+
 function sendEmail() {
+    const date = new Date()
+    const time = date.getTime()
+
     var name = document.getElementById("name").value
     var subject = document.getElementById("subject").value
     var body  = document.getElementById("body").value
     var contactEmail  = document.getElementById("contactEmail").value
 
-    if (subject != "" && body != "") {
-        if (contactEmail == "") {
-            contactEmail = "Prefer not to say"
-        }
+    if (localStorage.getItem("lastEmail") != null) {
+        console.log(time - localStorage.getItem("lastEmail"))
+    }
+    
+    if (localStorage.getItem("lastEmail") == null || time - localStorage.getItem("lastEmail") >= convMinToMilli(10)) {
+        if (subject != "" && body != "") {
+            if (contactEmail == "") {
+                contactEmail = "Prefer not to say"
+            }
 
-        if (name == "") {
-            name = "Prefer not to say"
-        }
+            if (name == "") {
+                name = "Prefer not to say"
+            }
 
-        if (body.length > 50) {
-            Email.send({
-                Host: "smtp.elasticemail.com",
-                Username : "ossyence@gmail.com",
-                Password : "59B00F3B623A4258354302AF327F4B7890E2",
-                To : 'ossyence.emails.recipient@gmail.com',
-                From : 'ossyence.emails@gmail.com',
-                Subject : subject,
-                Body : "<h3>" + body + "</h3><h2><br><br>By: " + name + "<br>Email: " + contactEmail + "<h2>"
-            }).then(
-                message => alert(message)
-            );
+            if (body.length > 50) {
+                Email.send({
+                    Host: "smtp.elasticemail.com",
+                    Username : "ossyence@gmail.com",
+                    Password : "59B00F3B623A4258354302AF327F4B7890E2",
+                    To : 'ossyence.emails.recipient@gmail.com',
+                    From : 'ossyence.emails@gmail.com',
+                    Subject : subject,
+                    Body : "<h3>" + body + "</h3><h2><br><br>By: " + name + "<br>Email: " + contactEmail + "<h2>"
+                }).then(
+                    message => alert(message)
+                );
+
+                localStorage.setItem("lastEmail", time)
+            }
+            else {
+                alert("Please make the body longer. (min 50 characters)")
+            }
         }
         else {
-            alert("Please make the body longer. (min 50 characters)")
+            alert("You need to actually fill in the subject and body.")
         }
     }
     else {
-        alert("You need to actually fill in the subject and body.")
+        alert("You can only send an email every 10 minutes.")
     }
 }
 
